062cc1d9425ee3e9556f3537b31c78ae72b29629
spandan1460 <1929250@kiit.ac.in>
2022-10-16 20:48:44 +0530

    Updated Video 8 Lecture

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -5,6 +5,10 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange = (event) => {
     setText(event.target.value)
   }
@@ -13,12 +17,22 @@ export default function TextForm(props) {
   // text = "new Text" //Wrong way to change the state of the text.
   // setText("new Text"); //Correct way to change the state of the text.
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleLowClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+      <h2>Your text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
