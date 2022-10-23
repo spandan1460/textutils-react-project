59c35fb094a99927ba4bac402a02c72b4aae8f11
spandan1460 <1929250@kiit.ac.in>
2022-10-23 22:15:32 +0530

    Updated lecutres till Video 11

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -9,6 +9,16 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText);
   }
+  const handleClearClick = () => {
+    setText("Enter the text here");
+  }
+  const handleCopy = () => {
+    console.log("I am copied");
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
   const handleOnChange = (event) => {
     setText(event.target.value)
   }
@@ -24,7 +34,9 @@ export default function TextForm(props) {
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-      <button className="btn btn-primary" onClick={handleLowClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear the Text</button>
+      <button className="btn btn-primary" onClick={handleCopy}>Copy the Text</button>
     </div>
     <div className="container my-3">
       <h2>Your text Summary</h2>
