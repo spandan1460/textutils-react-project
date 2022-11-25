2cbca85c9dea583cdd346d1c317dcfdb4ec69f7f
spandan1460 <1929250@kiit.ac.in>
2022-11-25 12:40:42 +0530

    Added lecture of video 12

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -23,27 +23,27 @@ export default function TextForm(props) {
     setText(event.target.value)
   }
 
-  const [text, setText] = useState('Enter text here');
+  const [text, setText] = useState('');
   // text = "new Text" //Wrong way to change the state of the text.
   // setText("new Text"); //Correct way to change the state of the text.
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} placeholder={"Enter text here"} onChange={handleOnChange} style={{backgroundColor:props.mode==='light'?'white':'grey', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
       <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear the Text</button>
       <button className="btn btn-primary" onClick={handleCopy}>Copy the Text</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
       <h2>Your text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes to read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
     </div>
     </>
   )
