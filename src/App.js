34bb6a58d6d17422e4ea7f10cdc75c7185ec88cd
spandan1460 <1929250@kiit.ac.in>
2023-01-21 13:44:49 +0530

    Update Video 15(Dynamic titles)

--- src/App.js
+++ src/App.js
@@ -25,11 +25,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been enabled","success");
+      document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
     }
   }
 
